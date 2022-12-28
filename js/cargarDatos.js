84c7ebd5e824953465695558b8e395f80463883e
GnZ7 <gonzalo62924hotmail.com>
2022-12-28 20:57:25 -0300

    Agrego nav tabs, archivo css

--- js/cargarDatos.js
+++ js/cargarDatos.js
@@ -7,8 +7,10 @@ function cargarDatos() {
   apellido = infoPersonal.name.last;
   foto = infoPersonal.picture.large;
   
-  let htmlFoto = " <img class = 'rounded-circle' src = '" + foto + "'>";
+  let htmlFoto = " <img class = 'rounded-circle' src = '" + foto + "'alt='Foto personal'>";
+  
   //$('#imagen').append(htmlInfo);
-  document.getElementById("imagen").innerHTML = htmlFoto;   
+  document.getElementById("imagen").innerHTML = htmlFoto;
+  document.getElementById("nombre").innerText = nombre + " " + apellido; 
 
  }
