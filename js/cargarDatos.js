e0af12911e93440dbfed114bcda55b6bb7422ed0
GNZ7 <gonzalo6292@hotmail.com>
2022-12-29 15:40:28 -0300

    Iconos del nav, continúo cargando la respues del api al html.

--- js/cargarDatos.js
+++ js/cargarDatos.js
@@ -1,16 +1,31 @@
-let nombre;
-let apellido;
+let nombreApellido;
 let foto;
+let email;
+let cel;
+let tel;
+let infoExtra;
 
 function cargarDatos() {
-  nombre = infoPersonal.name.first;
-  apellido = infoPersonal.name.last;
+  nombreApellido = infoPersonal.name.first + " " + infoPersonal.name.last;
+  email = infoPersonal.email;
   foto = infoPersonal.picture.large;
-  
   let htmlFoto = " <img class = 'rounded-circle' src = '" + foto + "'alt='Foto personal'>";
+  cel = infoPersonal.cell;
+  tel= infoPersonal.phone;
+  let locationTag = infoPersonal.location;
+  ubicacion = locationTag.street.number + " " + locationTag.street.name + " - " + locationTag.city + ", " + locationTag.country;
+  infoExtra = "I'm a " + infoPersonal.dob.age + " years old " + infoPersonal.gender + ", " + "born in " + infoPersonal.nat; 
   
-  //$('#imagen').append(htmlInfo);
   document.getElementById("imagen").innerHTML = htmlFoto;
-  document.getElementById("nombre").innerText = nombre + " " + apellido; 
-
+  document.getElementById("nombre").innerText = nombreApellido;
+  document.getElementById("email").innerText = email;
+  document.getElementById("cel").innerText = "Cel: " + cel;
+  document.getElementById("tel").innerText = "Tel: " + tel;
+  document.getElementById("ubicacion").innerText = ubicacion;
+  document.getElementById("infoExtra").innerText = infoExtra;
+  /**let htmlInfoExtra="";
+  infoExtra.forEach( element =>
+    htmlInfoExtra += '<li>' + element + '</>'
+  );
+  document.getElementById("ulInfoExtra").innerHTML = htmlInfoExtra;*/ 
  }
