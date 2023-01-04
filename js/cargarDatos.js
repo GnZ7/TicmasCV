d50581408ff83103431871394797b84f8a022152
GnZ7 <gonzalo62924hotmail.com>
2023-01-04 12:59:06 -0300

    Ajusto container para abarcar todo el largo de pantalla; agrego columna Sobre mi; cambio margenes y color de texto.

--- js/cargarDatos.js
+++ js/cargarDatos.js
@@ -3,7 +3,6 @@ let foto;
 let email;
 let cel;
 let tel;
-let infoExtra;
 
 function cargarDatos() {
   nombreApellido = infoPersonal.name.first + " " + infoPersonal.name.last;
@@ -11,21 +10,15 @@ function cargarDatos() {
   foto = infoPersonal.picture.large;
   let htmlFoto = " <img class = 'rounded-circle' src = '" + foto + "'alt='Foto personal'>";
   cel = infoPersonal.cell;
-  tel= infoPersonal.phone;
+  tel = infoPersonal.phone;
   let locationTag = infoPersonal.location;
   ubicacion = locationTag.street.number + " " + locationTag.street.name + " - " + locationTag.city + ", " + locationTag.country;
-  //infoExtra = "I'm a " + infoPersonal.dob.age + " years old " + infoPersonal.gender + ", " + "born in " + infoPersonal.nat; 
-  
+
   document.getElementById("imagen").innerHTML = htmlFoto;
   document.getElementById("nombre").innerText = nombreApellido;
   document.getElementById("email").innerText = email;
   document.getElementById("cel").innerText = "Cel: " + cel;
   document.getElementById("tel").innerText = "Tel: " + tel;
   document.getElementById("ubicacion").innerText = ubicacion;
-  //document.getElementById("infoExtra").innerText = infoExtra;
-  /**let htmlInfoExtra="";
-  infoExtra.forEach( element =>
-    htmlInfoExtra += '<li>' + element + '</>'
-  );
-  document.getElementById("ulInfoExtra").innerHTML = htmlInfoExtra;*/ 
- }
+
+}
